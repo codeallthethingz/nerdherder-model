c69134c5f751ee2479abc200fbdff1e58b3574ec
codeallthethingz <howdoicontactwill@gmail.com>
2020-10-12 11:40:26 -0700

    added competencies

--- user.go
+++ user.go
@@ -1,10 +1,16 @@
 package model
 
 type User struct {
-	Login       string `json:"login"`
-	NodeID      string `json:"node_id"`
-	AvatarURL   string `json:"avatar_url"`
-	Name        string `json:"name"`
-	Token       string `json:"token"`
-	AccessLevel string `json:"accessLevel"`
+	Login                string                         `json:"login"`
+	NodeID               string                         `json:"node_id"`
+	AvatarURL            string                         `json:"avatar_url"`
+	Name                 string                         `json:"name"`
+	Token                string                         `json:"token,omitempty"`
+	AccessLevel          string                         `json:"accessLevel"`
+	CompetenciesInMotion map[string]*CompetencyInMotion `json:"competenciesInMotion"`
+}
+
+type CompetencyInMotion struct {
+	CompetencyID string            `json:"competencyId"`
+	Status       map[string]string `json:"status"`
 }
